fb4c9022f2850da3d4427255a93b425a281cac91
ArinaBogan <arina.bogan@gmail.com>
2024-03-04 12:05:49 +0300

    added pagination

--- React-FilterBlocks/src/App.jsx
+++ React-FilterBlocks/src/App.jsx
@@ -1,15 +1,15 @@
 import Home from "./pages/Home/Home";
 import Vacancy from "./pages/Vacancy/Vacancy";
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/:id' element={<Vacancy />}></Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/:id' element={<Vacancy />}></Route>
+      </Routes>
     </>
   );
 }
--- React-FilterBlocks/src/components/Vacancies/Vacancies.jsx
+++ React-FilterBlocks/src/components/Vacancies/Vacancies.jsx
@@ -3,14 +3,21 @@ import { IconMapPin } from "@tabler/icons-react";
 import { Pagination } from '@mantine/core';
 import array from '../../storage/storage.json'
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 function Vacancies() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [vacanciesPerPage] = useState(5);
+
+    const lastIndex = vacanciesPerPage * currentPage;
+    const firstIndex = lastIndex - vacanciesPerPage;
+    const currentVacancies = array.slice(firstIndex, lastIndex);
     return (<>
         <div className={style.vacancies}>
             <div className={style.list}>
 
-                <div>{array.map((el) =>
-                    <Link to={`/${el.id}`}>
+                <div>{currentVacancies.map((el) =>
+                    <Link key={el.id} to={`/${el.id}`}>
                         <div key={el.id} className={style.content}>
                             <h1>{el.title}</h1>
                             <div className={style.text}>
@@ -25,7 +32,9 @@ function Vacancies() {
                     </Link>)}
                 </div>
             </div>
-            <Pagination total={10} />
+            <Pagination
+                total={Math.ceil(array.length / currentPage)}
+                onChange={setCurrentPage} />
         </div>
     </>);
 }
